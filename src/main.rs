cf85dc5897db8f013709d19d2a265e2190574320
Eee-3 <210351605+Eee-3@users.noreply.github.com>
2025-06-26 23:01:22 +0800

    fix:well no " is needed as runas crate has handled it

--- src/main.rs
+++ src/main.rs
@@ -85,11 +85,13 @@ fn install_rustdesk(config: &Config) -> Result<(), Box<dyn std::error::Error>> {
     info!("Installing Rustdesk from: {}", config.installer_filename);
     let current_dir = std::env::current_dir()?;
     info!("Current dir: {}", current_dir.display());
-    let install_command = format!(r#"cd /d "{}" && "{}" --silent-install"#, current_dir.display(),config.installer_filename);
+    // let install_command = format!(r#"cd /d "{}" && "{}" --silent-install"#, current_dir.display(),config.installer_filename);
+    let install_command = format!(r#"cd /d {} && {} --silent-install"#, current_dir.display(),config.installer_filename);
     debug!("install_command: {}", install_command);
     let status = Command::new("cmd")
         .arg("/c")
         .arg(&install_command)
+        .arg("&pause")
         .status()?;
     if !status.success() {
         error!("Failed to install Rustdesk: {}", status);
